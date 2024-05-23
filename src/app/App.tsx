0957e6538dde756324e2b5a10f2b9eae5f9137cf
Anthony Shaga <antonyshaga@gmail.com>
2024-05-23 18:34:23 +0300

    first commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -18,7 +18,7 @@ import {TaskTypeAPI} from "../api/todolist-api";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
 import {Login} from "../features/Login/Login";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {TodolistsList} from "../features/TodolistsList/TodolistsList";
 
 
@@ -100,6 +100,8 @@ export function App() {
                 <Routes>
                     <Route path={'/'} element={ <TodolistsList/>}/>
                     <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/404'} element={<h1>404 : PAGE NOT FOUND</h1>}/>
+                    <Route path={'*'} element={<Navigate to={'/404'}/>}/>
                 </Routes>
             </Container>
         </div>
